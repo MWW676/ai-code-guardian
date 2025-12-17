c9af27f5c1fa8b8cd934779c2d973adb1af6731d
fang <fang9761@gmail.com>
2025-12-17 16:32:33 +0800

    update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import logging
 from src.providers.gemini_client import GeminiClient
 from src.providers.s3_client import S3Uploader
+from test_data.github_webhook_event import GITHUB_WEBHOOK_EVENT
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -17,47 +18,66 @@ def lambda_handler(event, context=None):
             'body': json.dumps({'error': 'Missing event body.'})
         }
 
-    diff_data = json.loads(event_body).get('diff_text')
-    if not diff_data:
-        logger.error("No diff_text provided in the event.")
+    # diff_data = json.loads(event_body).get('diff_text')
+    # if not diff_data:
+    #     logger.error("No diff_text provided in the event.")
+    #     return {
+    #         'statusCode': 400,
+    #         'body': json.dumps({'error': 'Missing diff_text.'})
+    #     }
+
+    repo_full_name = json.loads(json.loads(event_body).get('repository')).get('full_name')
+    if not repo_full_name:
+        logger.error("No repo_full_name provided in the event.")
         return {
             'statusCode': 400,
-            'body': json.dumps({'error': 'Missing diff_text.'})
+            'body': json.dumps({'error': 'Missing repo_full_name.'})
         }
 
-    try:
-        provider = GeminiClient()
-        logger.info("Starting AI analysis ...")
-        resp = provider.analyze_diff(contents=diff_data)
-
-        if resp.get('status') in ['ERROR', 'SKIPPED']:
-            logger.warning(f'Analysis skipped or errored: {resp}')
-            return {
-                'statusCode': 200,
-                'body': json.dumps(resp)
-            }
-
-        uploader = S3Uploader()
-        upload_success = uploader.save_report(resp)
-
-        if not upload_success:
-            logger.error("Failed to upload report to S3.")
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': 'S3 Upload Failed.'})
-            }
+    pull_request_number = json.loads(event_body).get('diff_text')
+    if not pull_request_number:
+        logger.error("No pull_request_number provided in the event.")
         return {
-            'statusCode': 200,
-            'body': json.dumps({
-                'message': 'Analysis complete.',
-                's3_upload': 'Success',
-                'data': resp
-            })
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Missing pull_request_number.'})
         }
 
-    except Exception as e:
-        logger.error(f"Critical execution error:{str(e)}", exc_info=True)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+    # try:
+    #     provider = GeminiClient()
+    #     logger.info("Starting AI analysis ...")
+    #     resp = provider.analyze_diff(contents=diff_data)
+    #
+    #     if resp.get('status') in ['ERROR', 'SKIPPED']:
+    #         logger.warning(f'Analysis skipped or errored: {resp}')
+    #         return {
+    #             'statusCode': 200,
+    #             'body': json.dumps(resp)
+    #         }
+    #
+    #     uploader = S3Uploader()
+    #     upload_success = uploader.save_report(resp)
+    #
+    #     if not upload_success:
+    #         logger.error("Failed to upload report to S3.")
+    #         return {
+    #             'statusCode': 500,
+    #             'body': json.dumps({'error': 'S3 Upload Failed.'})
+    #         }
+    #     return {
+    #         'statusCode': 200,
+    #         'body': json.dumps({
+    #             'message': 'Analysis complete.',
+    #             's3_upload': 'Success',
+    #             'data': resp
+    #         })
+    #     }
+    #
+    # except Exception as e:
+    #     logger.error(f"Critical execution error:{str(e)}", exc_info=True)
+    #     return {
+    #         'statusCode': 500,
+    #         'body': json.dumps({'error': str(e)})
+    #     }
+
+if __name__ == "__main__":
+    lambda_handler(event=GITHUB_WEBHOOK_EVENT)
