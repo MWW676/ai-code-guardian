05bf23d9235b19c63a1a7fdd931cdfb794d478e7
fang <fang9761@gmail.com>
2025-12-17 19:06:45 +0800

    update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,12 +2,12 @@ import json
 import logging
 from src.providers.gemini_client import GeminiClient
 from src.providers.s3_client import S3Uploader
-from test_data.github_webhook_event import GITHUB_WEBHOOK_EVENT
+from src.providers.github_client import GithubClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def lambda_handler(event, context=None):
+def lambda_handler(event, context):
     # print event for debug ease
     logger.info(f"Received event: {event}")
     event_body = event.get('body')
@@ -34,42 +34,58 @@ def lambda_handler(event, context=None):
             'body': json.dumps({'error': 'Missing pull_request_number.'})
         }
 
-    # try:
-    #     provider = GeminiClient()
-    #     logger.info("Starting AI analysis ...")
-    #     resp = provider.analyze_diff(contents=diff_data)
-    #
-    #     if resp.get('status') in ['ERROR', 'SKIPPED']:
-    #         logger.warning(f'Analysis skipped or errored: {resp}')
-    #         return {
-    #             'statusCode': 200,
-    #             'body': json.dumps(resp)
-    #         }
-    #
-    #     uploader = S3Uploader()
-    #     upload_success = uploader.save_report(resp)
-    #
-    #     if not upload_success:
-    #         logger.error("Failed to upload report to S3.")
-    #         return {
-    #             'statusCode': 500,
-    #             'body': json.dumps({'error': 'S3 Upload Failed.'})
-    #         }
-    #     return {
-    #         'statusCode': 200,
-    #         'body': json.dumps({
-    #             'message': 'Analysis complete.',
-    #             's3_upload': 'Success',
-    #             'data': resp
-    #         })
-    #     }
-    #
-    # except Exception as e:
-    #     logger.error(f"Critical execution error:{str(e)}", exc_info=True)
-    #     return {
-    #         'statusCode': 500,
-    #         'body': json.dumps({'error': str(e)})
-    #     }
+    # Call Github API to retrieve git diff
+    github_client = GithubClient()
+    try:
+        diff_data = github_client.get_diff(repo_full_name=repo_full_name, pr_number=int(pull_request_number))
+        if diff_data == 'ERROR':
+            logger.warning("Encounter github API error when retrieving git diff for analysis.")
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': "Github API error."})
+            }
 
-if __name__ == "__main__":
-    lambda_handler(event=GITHUB_WEBHOOK_EVENT)
+    except Exception as e:
+        logger.error(f"Critical execution error:{str(e)}", exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
+    # Call Gemini API to perform analysis on git diff
+    try:
+        provider = GeminiClient()
+        logger.info("Starting AI analysis ...")
+        resp = provider.analyze_diff(contents=diff_data)
+
+        if resp.get('status') in ['ERROR', 'SKIPPED']:
+            logger.warning(f'Analysis skipped or errored: {resp}')
+            return {
+                'statusCode': 200,
+                'body': json.dumps(resp)
+            }
+
+        uploader = S3Uploader()
+        upload_success = uploader.save_report(resp)
+
+        if not upload_success:
+            logger.error("Failed to upload report to S3.")
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': 'S3 Upload Failed.'})
+            }
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'Analysis complete.',
+                's3_upload': 'Success',
+                'data': resp
+            })
+        }
+
+    except Exception as e:
+        logger.error(f"Critical execution error:{str(e)}", exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
