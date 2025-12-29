1bcf1d236ab68f99b9c945e6c1813924f0456d31
fang <fang9761@gmail.com>
2025-12-29 21:25:07 +0800

    update producer

--- src/producer.py
+++ src/producer.py
@@ -1,24 +1,21 @@
 import json
 import logging
-from src.providers.gemini_client import GeminiClient
-from src.providers.s3_client import S3Uploader
-from src.providers.github_client import GithubClient
-from src.utils.markdown_utils import format_report_to_markdown
 from src.utils.security_utils import verify_github_signature
 from src.utils.ssm_config_paths import *
 import boto3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 ssm_client = boto3.client('ssm')
+sqs_client = boto3.client('sqs')
 
 def get_configs():
     try:
         names = [
-            GEMINI_API_KEY,
             GITHUB_WEB_SECRET,
             GITHUB_API_KEY,
-            S3_BUCKET_NAME
+            SQS_URL
         ]
         resp = ssm_client.get_parameters(Names=names, WithDecryption=True)
         return {p['Name']: p['Value'] for p in resp['Parameters']}
@@ -68,44 +65,16 @@ def lambda_handler(event, context):
         logger.error("Missing repo name or PR number metadata.")
         return {'statusCode': 400, 'body': json.dumps({'error': 'Bad payload.'})}
 
-    # Fetch diff and perform analysis
+    # Pack and send msg to SQS
+    msg = {'repo_full_name': repo_full_name, 'pr_number': pull_request_number}
+
     try:
-        # Call Github API to fetch diff
-        logger.info(f"Fetching Git diff for {repo_full_name} PR #{pull_request_number}...")
-        github_client = GithubClient(api_key=configs.get(GITHUB_API_KEY))
-        diff_data = github_client.get_diff(repo_full_name=repo_full_name, pr_number=pull_request_number)
-        if diff_data == 'ERROR':
-            return {'statusCode': 502, 'body': json.dumps({'error': "Failed to fetch diff from Github."})}
-
-        # Call Gemini API to perform analysis
-        logger.info("Starting AI analysis ...")
-        provider = GeminiClient(api_key=configs.get(GEMINI_API_KEY))
-        resp = provider.analyze_diff(contents=diff_data)
-
-        if resp.get('status') in ['ERROR', 'SKIPPED']:
-            logger.warning(f'Analysis skipped or errored: {resp}')
-            return {'statusCode': 200, 'body': json.dumps(resp)}
-
-        # Generate report
-        markdown_report = format_report_to_markdown(report_data=resp)
-
-        # Call Github API to post report as PR comment
-        logger.info(f"Posting comment for {repo_full_name} PR#{pull_request_number}...")
-        post_success = github_client.post_comment(repo_full_name=repo_full_name, pr_number=pull_request_number, pr_comments=markdown_report)
-        if not post_success:
-            logger.error("⚠️ Failed to post comment to GitHub, but proceeding to S3 upload.")
-
-        # Store report results
-        uploader = S3Uploader(bucket_name=configs.get(S3_BUCKET_NAME))
-        upload_success = uploader.save_report(resp)
-
-        resp_body = {
-            'message': 'Analysis complete.',
-            's3_upload': 'Success' if upload_success else 'Failed',
-            'github_comment': 'Success' if post_success else 'Failed',
-            'data': resp
-        }
-        return {'statusCode': 200, 'body': json.dumps(resp_body)}
+        resp = sqs_client.send_message(
+            QueueUrl=configs.get(SQS_URL),
+            MessageBody=json.dumps(msg)
+        )
+        logger.info(f"Message sent! ID: {resp['MessageId']}")
+        return {'statusCode': 200, 'body': 'Request queued.'}
 
     except Exception as e:
         logger.error(f"Critical execution error:{str(e)}", exc_info=True)
